ddd9007b6f078ab0d0ee593d352a80e1e1e13c01
kol crooks <kolcrooks10@gmail.com>
2017-09-09 19:17:06 -0400

    Updates

--- src/com/NemianStudios/Util/MapReader.java
+++ src/com/NemianStudios/Util/MapReader.java
@@ -1,4 +1,73 @@
 package com.NemianStudios.Util;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 public class MapReader {
+
+
+    public boolean readFile(String s) {
+        Enumeration<? extends ZipEntry> zipentries;
+        try {
+            zipentries = ZipReader(s);
+        } catch (IOException e) {
+            return false;
+        }
+
+        while (zipentries.hasMoreElements()) {
+            ZipEntry entry = zipentries.nextElement();
+            entry.getName();
+        }
+        return true;
+    }
+
+    public Enumeration<? extends ZipEntry> ZipReader(String uri) throws IOException {
+        ZipFile zipFile = new ZipFile(uri);
+        return zipFile.entries();
+    }
+
+    private void readXMLFile() {
+        try {
+
+            File fXmlFile = new File("/Users/mkyong/staff.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("staff");
+
+            System.out.println("----------------------------");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                Node nNode = nList.item(temp);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("Staff id : " + eElement.getAttribute("id"));
+                    System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
+                    System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
+                    System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
+                    System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
